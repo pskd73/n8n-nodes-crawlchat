731e769fa5683040610fbb1d9e68ef0cdc99b3b1
Pramod <pramodkumar.damam73@gmail.com>
2025-10-26 19:50:31 +0530

    Add Page operation

--- nodes/CrawlChat/Crawlchat.node.ts
+++ nodes/CrawlChat/Crawlchat.node.ts
@@ -1,4 +1,8 @@
-import { type INodeType, type INodeTypeDescription, type ILoadOptionsFunctions } from 'n8n-workflow';
+import {
+	type INodeType,
+	type INodeTypeDescription,
+	type ILoadOptionsFunctions,
+} from 'n8n-workflow';
 
 const baseURL = 'https://wings.crawlchat.app';
 
@@ -35,17 +39,21 @@ export class Crawlchat implements INodeType {
 						name: 'Answer',
 						value: 'answer',
 					},
+					{
+						name: 'Add Page',
+						value: 'page',
+					},
 				],
 				default: 'answer',
 				routing: {
 					request: {
 						method: 'POST',
-						
+
 						body: {
 							query: '={{$parameter.query}}',
 						},
 					},
-				}
+				},
 			},
 			{
 				displayName: 'Collection Name or ID',
@@ -53,14 +61,15 @@ export class Crawlchat implements INodeType {
 				type: 'options',
 				required: true,
 				default: '',
-				description: 'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>',
+				description:
+					'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>',
 				typeOptions: {
 					loadOptionsMethod: 'getCollections',
 				},
 				routing: {
 					request: {
-						url: '=/answer/{{$parameter.collection}}',
-					}
+						url: '=/{{$parameter.operation}}/{{$parameter.collection}}',
+					},
 				},
 			},
 			{
@@ -99,22 +108,134 @@ export class Crawlchat implements INodeType {
 						property: 'prompt',
 					},
 				},
-			}
+			},
+			{
+				displayName: 'Knowledge Group Name or ID',
+				name: 'knowledgeGroupId',
+				type: 'options',
+				default: '',
+				description: 'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>',
+				displayOptions: {
+					show: {
+						operation: ['page'],
+					},
+				},
+				typeOptions: {
+					loadOptionsMethod: 'getKnowledgeGroups',
+					loadOptionsDependsOn: ['collection'],
+				},
+				routing: {
+					send: {
+						type: 'body',
+						property: 'knowledgeGroupId',
+					},
+				},
+			},
+			{
+				displayName: 'Title',
+				name: 'title',
+				type: 'string',
+				default: '',
+				description: 'The title of the page',
+				displayOptions: {
+					show: {
+						operation: ['page'],
+					},
+				},
+				routing: {
+					send: {
+						type: 'body',
+						property: 'title',
+					},
+				},
+			},
+			{
+				displayName: 'Content',
+				name: 'content',
+				type: 'string',
+				default: '',
+				description: 'The content of the page',
+				displayOptions: {
+					show: {
+						operation: ['page'],
+					},
+				},
+				routing: {
+					send: {
+						type: 'body',
+						property: 'content',
+					},
+				},
+			},
+			{
+				displayName: 'Page Key',
+				name: 'key',
+				type: 'string',
+				default: '',
+				description: 'The key of the page',
+				displayOptions: {
+					show: {
+						operation: ['page'],
+					},
+				},
+				routing: {
+					send: {
+						type: 'body',
+						property: 'key',
+					},
+				},
+			},
 		],
 	};
 
 	methods = {
 		loadOptions: {
-			async getCollections(this: ILoadOptionsFunctions): Promise<Array<{ name: string; value: string }>> {
-				const response = await this.helpers.httpRequestWithAuthentication.call(this, 'crawlchatApi', {
-					method: 'GET',
-					url: `${baseURL}/collection`,
-				});
+			async getCollections(
+				this: ILoadOptionsFunctions,
+			): Promise<Array<{ name: string; value: string }>> {
+				const response = await this.helpers.httpRequestWithAuthentication.call(
+					this,
+					'crawlchatApi',
+					{
+						method: 'GET',
+						url: `${baseURL}/collection`,
+					},
+				);
 
 				return response.map((collection: { collectionName: string; collectionId: string }) => ({
 					name: collection.collectionName,
 					value: collection.collectionId,
-				}))
+				}));
+			},
+
+			async getKnowledgeGroups(
+				this: ILoadOptionsFunctions,
+			): Promise<Array<{ name: string; value: string }>> {
+				const collectionId = this.getNodeParameter('collection');
+				
+				if (!collectionId) {
+					return [];
+				}
+
+				const response = await this.helpers.httpRequestWithAuthentication.call(
+					this,
+					'crawlchatApi',
+					{
+						method: 'GET',
+						url: `${baseURL}/collection`,
+					},
+				);
+
+				for (const collection of response) {
+					if (collection.collectionId === collectionId) {
+						return collection.knowledgeGroups?.map((group: { title: string; id: string }) => ({
+							name: group.title,
+							value: group.id,
+						})) || [];
+					}
+				}
+
+				return [];
 			},
 		},
 	};
